0493a81d5100fa641e305df87cef5b26321094f2
veneracoding <otaboyevashahzoda0405@gmail.com>
2025-11-06 15:15:39 +0500

    initial commit: countries project

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path=require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports={
+    // mode
+    mode: 'development', //production
+    // entry
+    entry: {
+        main: path.resolve(__dirname, 'src/js/main.js'),
+        about: path.resolve(__dirname, 'src/js/about.js'),
+
+    },
+    // output
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name][contenthash].js',
+        clean: true,
+    },
+    // devServer
+    // loader
+    // plugins
+    plugins: [
+       new HtmlWebpackPlugin({
+        title: "Countires | Home",
+        template: './src/indexTemp.html',
+        filename: 'index.html',
+        chunks: ['main']
+       }),
+       
+       new HtmlWebpackPlugin({
+        title: "Countires | About",
+        template: './src/pages/aboutTemp.html',
+        filename: 'about.html',
+        chunks: ['about']
+       })
+    ]
+}
