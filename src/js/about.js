5a47e67a5f4dfddf2038ba98b962e1a6b70033c9
veneracoding <otaboyevashahzoda0405@gmail.com>
2025-11-14 14:54:48 +0500

    borders api, flags and new information

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,20 @@
 import '../css/main.css'
+import './mode'
+
+import request from './request'
+import { createCountryInfo } from './updateUI'
+
+const params = new URLSearchParams(window.location.search)
+const country = params.get('country')
+
+console.log(country)
+
+const countryAPI = `https://restcountries.com/v3.1/name/${country}`
+
+console.log(countryAPI)
+
+request(countryAPI)
+    .then(data => {
+        createCountryInfo(data[0])
+    })
+    .catch(err => alert(err.message))
--- src/js/updateUI.js
+++ src/js/updateUI.js
@@ -15,19 +15,21 @@ export const createCountries = (countries) => {
         const capital = country.capital ? country.capital[0] : "No Capital"
         const flag = country.flags?.svg || country.flags?.png
 
-        //li 
+        
+         // li
         const li = document.createElement('li')
         li.classList.add('cards_item')
+
         li.innerHTML = `
-         <a href="./about.html">
-           <img src="${flag}" alt="${commonName}-flag" width="267" height="160">
-            <div class="cards__item-inner">
-                <h3 class="cards__title">${commonName}</h3>
-                <p class="population">Population: <span>${population}</span></p>
-                <p class="region">Region: <span>${region}</span></p>
-                <p class="capital">Capital: <span>${capital}</span></p>
-            </div>
-         </a>
+            <a href="./about.html?country=${commonName}">
+                <img src="${flag}" alt="${commonName}-flag" width="267" height="160">
+                <div class="cards__item-inner">
+                    <h3 class="cards__title">${commonName}</h3>
+                    <p class="population">Population: <span>${population}</span></p>
+                    <p class="region">Region: <span>${region}</span></p>
+                    <p class="capital">Capital: <span>${capital}</span></p>
+                </div>
+            </a>
         `
  
         //ul 
@@ -36,13 +38,88 @@ export const createCountries = (countries) => {
     })
 }
 
-
+   
     /* <a href="./about.html">
-    <img src="./images/jpg/germany-flag.jpg" alt="germany-flag" width="267" height="160">
-    <div class="cards__item-inner">
-        <h3 class="cards__title">Germany</h3>
-        <p class="population">Population: <span>81,770,900</span></p>
-        <p class="region">Region: <span>Europe</span></p>
-        <p class="capital">Capital: <span>Berlin</span></p>
-    </div>
-    </a> */
+        <img src="./images/jpg/germany-flag.jpg" alt="germany-flag" width="267" height="160">
+        <div class="cards__item-inner">
+            <h3 class="cards__title">Germany</h3>
+            <p class="population">Population: <span>81,770,900</span></p>
+            <p class="region">Region: <span>Europe</span></p>
+            <p class="capital">Capital: <span>Berlin</span></p>
+        </div>
+        </a> */
+
+
+    //ABOUT UI
+
+    const countryInfoEl = document.querySelector('.country-info')
+
+    export const createCountryInfo = (country) => {
+        const{ population, borders, capital, flags, name, 
+        region, tld, currencies, languages,subregion} = country
+
+        const nativeName = Object.values(name.nativeName)[0].official
+        const currency = Object.values(currencies)[0].name
+        const language = Object.values(languages)
+
+        countryInfoEl.innerHTML = `
+        
+            <img
+              class="country-info__img"
+              src=${flags.svg}
+              alt="germany-flag"
+              width="560"
+              height="400"
+            />
+            <div class="country-info__content">
+              <h2>${name.common}</h2>
+              <ul class="country-info__list">
+                <li class="country-info__item">
+                  <p class="name">
+                    Native Name:
+                    <span>${nativeName}</span>
+                  </p>
+                  <p class="population">
+                    Population:
+                    <span>${population}</span>
+                  </p>
+                  <p class="region">
+                    Region:
+                    <span>${region}</span>
+                  </p>
+                  <p class="sub-region">
+                    Sub Region:
+                    <span>${subregion}</span>
+                  </p>
+                  <p class="capital">
+                    Capital:
+                    <span>${capital}</span>
+                  </p>
+                </li>
+                <li class="country-info__item">
+                  <p class="name">
+                    Top Level Domain:
+                    <span>${tld}</span>
+                  </p>
+                  <p class="population">
+                    Currencies:
+                    <span>${currency}</span>
+                  </p>
+                  <p class="region">
+                    Languages:
+                    <span>${language}</span>
+                  </p>
+                </li>
+              </ul>
+  
+              <div class="country-info__borders">
+                ${borders ? borders.map((border) => {
+                    return`
+                    <a href="./about.html?country=${border}">${border}</a> 
+                    `    
+                }): "No Borders"}
+              </div>
+            </div>
+          `
+    }
+
